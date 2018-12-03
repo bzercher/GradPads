9514eb3c8aa8c8f505d5526c0e242a66365a4007
Ben Zercher <bzercher@uw.edu>
2018-12-03 14:33:53 -0800

    Edited setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+PACKAGES = find_packages()
+
+setup(
+    name='GradPads',
+    version='0.1',
+    url='http://github.com/bzercher/GradPads',
+    license='MIT',
+    packages=PACKAGES,
+    author='Ben Zercher',
+    author_email='bzercher@uw.edu'
+)
